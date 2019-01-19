1816bc35ff15c657d733b8279ae70860dc06bd6b
tk0358 <tk0358a@yahoo.co.jp>
2019-01-20 06:55:20 +0900

    modified 05_03

--- chap5/05_03.rb
+++ chap5/05_03.rb
@@ -18,3 +18,20 @@ class Mechanic
 
   # ...
 end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+
+  #...
+end
+
+class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+  #...
+end
